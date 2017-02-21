a7f65103e4e0854930a5f8273f52e0179ad43994
Pedro Conchas <pdrocb@gmail.com>
2017-02-21 17:35:22 -0600

    add localstorage support

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,54 @@
+var expect = require('expect');
+
+var TodoAPI = require('TodoAPI');
+
+describe('TodoAPI', () => {
+  beforeEach(() => {
+    localStorage.removeItem('todos');
+  });
+
+  it('Should exist', () => {
+    expect(TodoAPI).toExist();
+  });
+
+  describe('setTodos', () => {
+    it('Should set valid todos array', () => {
+      var todos = [{
+        id: 23,
+        test: 'test all files',
+        completed: false
+      }];
+      TodoAPI.setTodos(todos);
+      var actualTodos = JSON.parse(localStorage.getItem('todos'));
+
+      // compares the value of objects or array
+      expect(actualTodos).toEqual(todos);
+    });
+
+    it('should not set invalid todos array', () => {
+      var badTodos = {a: 'b'};
+      TodoAPI.setTodos(badTodos);
+
+      expect(localStorage.getItem('todos')).toBe(null);
+    });
+  });
+
+  describe('getTodos', () => {
+    it('should return empty array for bad localStorage data', () => {
+      var actualTodos = TodoAPI.getTodos();
+      expect(actualTodos).toEqual([]);
+    });
+
+    it('should return todos if valid array in localStorage', () => {
+      var todos = [{
+        id: 23,
+        test: 'test all files',
+        completed: false
+      }];
+      localStorage.setItem('todos', JSON.stringify(todos));
+      var actualTodos = TodoAPI.getTodos();
+
+      expect(actualTodos).toEqual(todos);
+    });
+  });
+});
---
+++ app/tests/components/TodoApp.test.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoApp = require('TodoApp');
+
+describe('TodoApp', () => {
+  it('should exist', () => {
+    expect(TodoApp).toExist();
+  });
+
+  it('Should add Todo to the todos state on hanldeAddTodo', () => {
+    var todoText = 'test text';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
+
+  it('Should toggle completed value when handleToggle called', () => {
+    var todoData = {
+      id: 11,
+      text: 'Text features',
+      completed: false
+    };
+
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(true);
+  });
+});
