2d426d04b6262596a626eb98458c06851b359a67
Pedro Conchas <pdrocb@gmail.com>
2017-02-21 22:45:41 -0600

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,40 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filteredTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    // Wie will return items that have completed false but if showcompleted is check we're gonna return all
+    filteredTodos = filteredTodos.filter((todo) => {
+      // if true the current item gonna stay in the array
+      return !todo.completed || showCompleted;
+    });
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non-completed first
+    /*
+    sort modifies the current array, takes one argument which is a fn
+    if return -1 we are telling a should come before b
+    if we return 1 we are telling that b should come before a
+    if return 0 it's gonna be no change
+    */
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -15,7 +15,7 @@ describe('TodoAPI', () => {
     it('Should set valid todos array', () => {
       var todos = [{
         id: 23,
-        test: 'test all files',
+        text: 'test all files',
         completed: false
       }];
       TodoAPI.setTodos(todos);
@@ -42,7 +42,7 @@ describe('TodoAPI', () => {
     it('should return todos if valid array in localStorage', () => {
       var todos = [{
         id: 23,
-        test: 'test all files',
+        text: 'test all files',
         completed: false
       }];
       localStorage.setItem('todos', JSON.stringify(todos));
@@ -50,5 +50,47 @@ describe('TodoAPI', () => {
 
       expect(actualTodos).toEqual(todos);
     });
+
+    describe('filtered todos', () => {
+      var todos = [{
+        id: 1,
+        text: 'some text',
+        completed: true
+      }, {
+        id: 2,
+        text: 'text 02',
+        completed: false
+      }, {
+        id: 3,
+        text: 'some text 03',
+        completed: true
+      }];
+
+      it('should return all items if showCompleted is true', () => {
+        var filteredTodos = TodoAPI.filteredTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
+      it('should return the items that have been completed', () => {
+        var filteredTodos = TodoAPI.filteredTodos(todos, false, '');
+        expect(filteredTodos.length).toBe(1);
+      });
+
+      it('should sort by completed status', () => {
+        var filteredTodos = TodoAPI.filteredTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+      });
+
+      it('should filter todos by searchText', () => {
+        var filteredTodos = TodoAPI.filteredTodos(todos, true, 'some');
+        expect(filteredTodos.length).toBe(2);
+      });
+
+      it('should return all todos if searchText is empty', () => {
+        var filteredTodos = TodoAPI.filteredTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+    });
+
   });
 });
