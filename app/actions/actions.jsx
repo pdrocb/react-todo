0e7fa99b8c372829610e1719b280ad2750a5e048
Pedro Conchas <pdrocb@gmail.com>
2017-03-05 16:49:37 -0600

    add actions & action test

---
+++ app/actions/actions.jsx
@@ -0,0 +1,29 @@
+
+export var setSearchText = (searchText) => {
+  return {
+      type: 'SET_SEARCH_TEXT',
+      searchText
+  };
+};
+
+// Toggle showCompleted
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  };
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
+};
+
+// toggleTodo(id)
+export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,47 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('Should toggle completed todos', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate todo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Study hard'
+    };
+
+    var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should toggle todo by id', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: '123'
+    };
+
+    var res = actions.toggleTodo(action.id);
+
+    expect(res).toEqual(action);
+  });
+});
